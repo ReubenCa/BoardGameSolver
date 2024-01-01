1b50b64ee526e43b3470fb947da20cb4fe65f0f9
Reuben Carolan <reuben.carolan@gmail.com>
2024-01-01 21:42:37 +0000

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        //You add a number from -10 to 10 to your total and then a random number from 1-10 is added to your total 
+        //you win if you reach exactly 100
+        //you want to win as quick as possible
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Solver/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Solver
+{
+    public abstract class GameEvent
+    {
+        
+    }
+
+     
+    //Can Inherit from Random Event in Game Specific Project if more complex logic is needed
+    //T is the type of GameState
+    public class RandomEvent : GameEvent
+    {
+
+    }
+
+    public interface IGameState
+    {
+        public bool equivalent(IGameState other);
+        public GameEvent getNextEvent();
+    }
+
+    //When search tree is too large this used as a heurisitic to score a position
+    public interface IStaticStateScorer<T> : IStateScorer<T> where T : IGameState
+    {
+        double approximateUtility(IGameState gameState);
+    }
+
+    public interface IStateScorer<T> where T : IGameState
+    {
+        bool exactScoreKnown(T gameState, out double exactUtility);
+       
+    }
+
+
+    //A single solver maintains its own cache, a new instance should be created whenver a seperate cache should be used (e.g. new scoring function)
+    public class Solver<T> where T : IGameState
+    {
+        Dictionary
+
+        Solver(IStateScorer<T> scorer)
+        { 
+            this.scorer = scorer;
+            if(scorer is IStaticStateScorer<T>)
+            {
+                staticscorer = (IStaticStateScorer<T>) scorer;
+            }
+            else
+            {
+                staticscorer = null;
+            }
+        }
+        
+        IStateScorer<T> scorer;
+        IStaticStateScorer<T>? staticscorer;
+
+       
+        public int chooseBestMove(T gameState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ScorePosition(IGameState gameState)
+        {
+            if(scorer.exactScoreKnown((T)gameState, out double exactScore))
+            {
+                return (double) exactScore;
+            }
+         
+           
+        }
+    }
+}
